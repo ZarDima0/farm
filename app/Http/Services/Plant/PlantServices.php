8e3e7b0b9321ff607521249e9a5357da341cff68
Dima Fibikh <dfibikhalex@gmail.com>
2022-11-13 14:50:41 +0700

    feat():cоздал миграцию

--- app/Http/Services/Plant/PlantServices.php
+++ app/Http/Services/Plant/PlantServices.php
@@ -2,13 +2,12 @@
 namespace App\Http\Services\Plant;
 
 use App\Models\Plant;
-use App\Models\User;
 
 class PlantServices
 {
 
     public function getList()
     {
-        return Plant::all();
+        return Plant::All();
     }
 }
