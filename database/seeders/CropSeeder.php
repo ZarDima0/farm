23ccb4a2d3094d600a47b77f56ea4f4383e52778
Dima Fibikh <dfibikhalex@gmail.com>
2022-11-20 19:15:25 +0700

    fix():исправил сиды для crop

--- database/seeders/CropSeeder.php
+++ database/seeders/CropSeeder.php
@@ -16,35 +16,6 @@ class CropSeeder extends Seeder
      */
     public function run()
     {
-        $plant = [
-            [
-                'plantable_type'=> 'App\Models\Tree',
-                'plantable_id'=> 1,
-                'name'=> 'ядро кедра',
-                'yield_per_tile'=> 30,
-            ],
-            [
-                'plantable_type'=> 'App\Models\Plant',
-                'plantable_id'=> 1,
-                'name'=> 'Зерно пшеницы',
-                'yield_per_tile'=> 30,
-            ],
-            [
-                'plantable_type'=> 'App\Models\Plant',
-                'plantable_id'=> 1,
-                'name'=> 'Ягода клубники',
-                'yield_per_tile'=> 200,
-            ]
-        ];
-
-        foreach ($plant as $item) {
-            Crop::create([
-                'name' => $item['name'],
-                'plantable_type' =>$item['plantable_type'],
-                'plantable_id' => $item['plantable_id'],
-                'yield_per_tile' => $item['yield_per_tile']
-            ]);
-        }
     }
 
 }
