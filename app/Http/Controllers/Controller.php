4803181e32f06eaf33368ca82424e1567bac6e57
Dima <dima.f@pyrobyte.ru>
2022-12-24 16:30:11 +0700

    feat: swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,25 +6,7 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-/**
- * @OA\Info(
- *     title="Grill1 API",
- *     version="1.0",
- * )
- * @OA\SecurityScheme(
- *      securityScheme="bearerAuth",
- *      in="header",
- *      name="bearerAuth",
- *      type="http",
- *      scheme="bearer",
- *      bearerFormat="JWT",
- * )
- *
- * @OA\Server(
- *      description="DEV Server API",
- *      url="https://grill1.dev.pyrobyte.ru/api/v1"
- * )
- */
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
